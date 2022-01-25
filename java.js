9012dd8d434d2c741c45e1e3934d5011861e29c1
Funmi-Amusan <81365548+Funmi-Amusan@users.noreply.github.com>
2022-01-25 21:36:47 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,79 @@
+/* Navigation Control for Mobile */
+const nav = document.querySelector("nav");
+const openNav = document.querySelector("#open-nav");
+
+openNav.addEventListener("click", () => {
+    nav.classList.add ('nav-active')
+
+})
+
+closeNav.addEventListener("click", () => {
+    nav.classList.remove ('nav-active')
+
+})
+
+
+
+/*------ Slider ------*/
+
+const slide1 = document.getElementById('slide1')
+const slide2 = document.getElementById('slide2')
+const slide3 = document.getElementById('slide3')
+
+const prevBtn = document.getElementById('prev')
+const nextBtn = document.getElementById('next')
+const prevBtn2 = document.getElementById('prev2')
+const nextBtn2 = document.getElementById('next2')
+const prevBtn3 = document.getElementById('prev3')
+const nextBtn3 = document.getElementById('next3')
+
+nextBtn.onclick = function() {
+    slide1.style.left = "-100%"
+    slide2.style.left = "0%"
+    slide3.style.left = "100%"
+}
+
+prevBtn.onclick = function() {
+    slide1.style.left = "100vw"
+    slide2.style.left = "-100%"
+    slide3.style.left = "0%"
+}
+
+nextBtn2.onclick = function() {
+    slide1.style.left = "-100%"
+    slide2.style.left = "100%"
+    slide3.style.left = "0%"
+}
+
+prevBtn2.onclick = function() {
+    slide1.style.left = "0%"
+    slide2.style.left = "-100%"
+    slide3.style.left = "100%"
+}
+
+nextBtn3.onclick = function() {
+    slide1.style.left = "0%"
+    slide2.style.left = "-100%"
+    slide3.style.left = "100%"
+}
+
+prevBtn3.onclick = function() {
+    slide1.style.left = "-100%"
+    slide2.style.left = "0%"
+    slide3.style.left = "100%"
+}
+
+/* button animation */
+
+const button = document.querySelectorAll('a');
+button.forEach ( btn =>  {
+    btn.addEventListener('click', function(e) {
+        let x = e.clientX = e.target.offsetLeft;
+        let y = e.clientY = e.target.offsetTop;
+
+        let ripples = document.createElement('span');
+        ripples.style.left = x + 'px';
+        ripples.style.top = y + 'px';
+        this.appendChild(ripples);
+    })
+})
